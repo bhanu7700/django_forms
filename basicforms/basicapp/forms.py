9dc6ac2cbe5fd22b2be23c42a2f5a60b4d94f0ce
bhanu7700 <bhanu.udhissa@gmail.com>
2020-05-16 09:30:06 +0530

    first commit

---
+++ basicforms/basicapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.core import validators
+
+class FormName(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    verify_mail = forms.EmailField(label='Enter your email again ')
+    text =  forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verify_mail']  
+
+        if email != vemail:
+            raise forms.ValidationError("make sure mails match")
+    
+                                 
+
---
+++ basicforms/basicapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from . import forms
+
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'basicapp/index.html')
+
+def form_view(request):
+
+    form = forms.FormName()
+
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+
+            print("Validation Successfull")
+            print("NAME:"+form.cleaned_data['name'])
+            print("EMAIL:"+form.cleaned_data['email'])
+            print("TEXT:"+form.cleaned_data['text'])
+
+
+
+
+
+
+        
+
+
+
+    return render(request,'basicapp/forms.html',{'form':form})    
